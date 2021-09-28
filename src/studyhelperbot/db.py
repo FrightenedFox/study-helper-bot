063d6e587cf94741c86edc92b4cdcb7760a7c54d
FrightenedFox <vitalij20358@gmail.com>
2021-09-28 19:32:48 +0200

    Add/remove user; get/set specific options.

--- src/studyhelperbot/db.py
+++ src/studyhelperbot/db.py
@@ -28,6 +28,69 @@ class AnalizaDB:
         if self.conn is not None:
             self.is_connected = True
 
+    def create_new_user_account(self, telegram_id, bot_chat_id):
+        """Creates new record in the `users` table"""
+        cur = self.conn.cursor()
+        cur.execute(f"INSERT INTO users (telegram_id, bot_chat_id) "
+                    f"VALUES (%(telegram_id)s, %(bot_chat_id)s);",
+                    {
+                        "telegram_id": telegram_id,
+                        "bot_chat_id": bot_chat_id,
+                    })
+        self.conn.commit()
+        cur.close()
+        logging.info(f"A new user account was created :: "
+                     f"telegram_id={telegram_id}; bot_chat_id={bot_chat_id}")
+
+    def delete_user_account(self, bot_chat_id=None, telegram_id=None):
+        """Removes user account from the table.
+        `bot_chat_id` or 'telegram_id' should be provided"""
+        if bot_chat_id is None and telegram_id is None:
+            raise Exception("bot_chat_id or telegram_id should be provided.")
+        elif telegram_id is not None:
+            where, value_id = 'telegram_id', telegram_id
+        else:
+            where, value_id = 'bot_chat_id', bot_chat_id
+
+        cur = self.conn.cursor()
+        cur.execute(f"DELETE FROM users "
+                    f"WHERE {where}=%s;", (value_id,))
+        self.conn.commit()
+        cur.close()
+        logging.info(f"The following user account was deleted :: {where}={value_id}")
+
+    def get_specific_column(self, where, where_value, col_name, table="users"):
+        """Retrieves a specific value from a specific row"""
+        cur = self.conn.cursor()
+        cur.execute(f"SELECT {col_name} "
+                    f"FROM   {table} "
+                    f"WHERE  {where} = %s;", (where_value,))
+        ans = cur.fetchone()
+        cur.close()
+        logging.debug(f"Run the following SQL query :: "
+                      f"SELECT {col_name} FROM {table} WHERE {where}={where_value}")
+        logging.debug(f"Result :: {ans}")
+        return ans
+
+    def set_specific_column(self, where, where_value,
+                            col_name, col_name_value,
+                            table="users"):
+        """Updates a specific value from a specific row"""
+        cur = self.conn.cursor()
+        cur.execute(
+                    f"UPDATE    {table} "
+                    f"SET       {col_name} = %(col_name_value)s "
+                    f"WHERE     {where} = %(where_value)s;",
+                    {
+                        "col_name_value": col_name_value,
+                        "where_value": where_value,
+                    }
+        )
+        self.conn.commit()
+        cur.close()
+        logging.debug(f"Run the following SQL query :: "
+                      f"UPDATE {table} SET {col_name}={col_name_value} WHERE {where}={where_value}")
+
     def get_expected_answer(self, bot_chat_id):
         """Retrieves `wait_for_answer` and `expected_answer` values
         for a specific `bot_chat_id` conversation."""
@@ -41,14 +104,15 @@ class AnalizaDB:
                       f"wait_for_answer={ans[0]}; expected_answer='{ans[1]}'")
         return ans
 
-    def set_expected_answer(self, bot_chat_id, wait_for_answer, expected_answer):
+    def set_expected_answer(self, bot_chat_id,
+                            wait_for_answer=False, expected_answer=None):
         """Updates `wait_for_answer` and `expected_answer` values
         for a specific `bot_chat_id` conversation."""
         cur = self.conn.cursor()
-        cur.execute(f"UPDATE users "
-                    f"SET   wait_for_answer = %(wait_for_ans)s, "
-                    f"      expected_answer = %(exp_ans)s  "
-                    f"WHERE bot_chat_id = %(bot_chat_id)s;",
+        cur.execute(f"UPDATE    users "
+                    f"SET       wait_for_answer = %(wait_for_ans)s, "
+                    f"          expected_answer = %(exp_ans)s  "
+                    f"WHERE     bot_chat_id = %(bot_chat_id)s;",
                     {
                         "wait_for_ans": wait_for_answer,
                         "exp_ans": expected_answer,
@@ -75,8 +139,14 @@ class AnalizaDB:
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(level=logging.DEBUG)
     db = AnalizaDB()
     db.connect()
-    print(db.get_expected_answer(bot_chat_id=98762378))
+    # print(db.get_expected_answer(bot_chat_id=98762378))
+    # db.set_expected_answer(bot_chat_id=219836423479)
+    # print(db.get_specific_column('user_id', 5, 'prz_email'))
+    # db.set_specific_column('user_id', 3, 'telegram_id', 1010)
+    # print(db.get_specific_column('user_id', 3, 'telegram_id'))
+    # db.create_new_user_account(1231122, 54352353)
+    # db.delete_user_account(telegram_id=1231122)
     db.disconnect()
